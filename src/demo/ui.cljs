fdbee3e40e2de308e72803f964b8684186520326
Niki Jiandani <niki.jiandani@shopify.com>
2022-10-05 21:19:10 -0400

    Add content from gist and fix emojis
    Co-authored-by: Recia <recia1@gmail.com>
    Co-authored-by: Raf <rafal.dittwald@gmail.com>

--- src/demo/ui.cljs
+++ src/demo/ui.cljs
@@ -1,16 +1,41 @@
 (ns demo.ui
- (:require
-    [reagent.core :as r]))
+  (:require
+   [reagent.core :as r]))
 
-(defonce state (r/atom {:counter 1}))
+(defonce state (r/atom {:game-board [[:x :o :x] [:o nil nil] [nil nil nil]]
+                        :current-player :x}))
+
+;; "how can i represent any moment of my app as a dictionary"
+(defn flip0 [player]
+  (if (= player :x)
+    :o
+    :x))
+
+(defn flip1 [player]
+  (case player
+    :x :o
+    :o :x))
+
+(def flip
+  {:x :o
+   :o :x})
 
 (defn app-view []
   [:div
-   [:h1 "Hello World!"]
-   (:counter @state)
-   [:button {:on-click (fn []
-                         (swap! state update :counter inc))}
-    "+1"]])
+   [:table
+    [:tbody
+      (for [[row-index row] (map-indexed vector (:game-board @state))]
+        [:tr
+         (for [[col-index item] (map-indexed vector row)]
+          [:td {:on-click (fn []
+                            (swap! state assoc-in [:game-board row-index col-index] (:current-player @state))
+                            (swap! state update :current-player flip))}
+           (case item
+                  :x "❌"
+                  :o "⭕"
+                  "⬜️")])])]]])
 
-#_(js/alert "yo")
-#_(+ 1 2)
+(def test-game-state
+  {:game-board   [[\x \o \x] [\o nil nil] [nil nil nil]]
+   :move-history [[[0 0] \x] [[0 1] \o] [[0 2] \x] [[1 0] \o]]
+   :current-move-index 3})
